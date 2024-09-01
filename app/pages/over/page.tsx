8f95b631ecec9156940942bffd1e0a87855c72ae
Mouloud Hasrane <m.hasrane@esi-sba.dz>
2024-09-01 23:33:40 +0100

    removed the search Params

--- app/pages/over/page.tsx
+++ app/pages/over/page.tsx
@@ -3,7 +3,6 @@ import { statsState, useStatsStore } from "@/app/state/store"
 import { useSearchParams } from "next/navigation"
 
 const Page = () => {
-  const searchParams=useSearchParams()
   const accuarcy=useStatsStore((state)=>state.accuarcy) 
   const wpm=useStatsStore((state)=>state.wpm)
   return (
